9dbc37c8ed0efa0c11065a716d12544842bb46e8
Yoonji Jang <yjang@Yoonjis-Macbook-Pro.local>
2017-05-12 12:53:23 +0900

    cube

---
+++ js/cube.js
@@ -0,0 +1,69 @@
+window.addEventListener('letsee.load', function() {
+  // Create an entity
+  var entity = new Entity('https://d.letsee.io/EMdTOnQqEjfW');
+  var cubeSize = 1200;
+  var margin = cubeSize / 2;
+
+  // Load entity data
+  entity.load().then(function(e) {
+    var cube = new Renderable();
+    var faces = document.getElementsByClassName('cube-face');
+
+    // Iterate each face of the cube
+    for (var i = 0; i < faces.length; i++) {
+      var cubeFace = new DOMRenderable(faces[i]);
+
+      switch (index) {
+        case 0: // video
+          cubeFace.rotateY(Math.PI / 2);
+          cubeFace.position.x = margin;
+          break;
+        case 1: // map
+          cubeFace.rotateY(-Math.PI / 2);
+          cubeFace.position.x = -margin;
+          break;
+        case 2: // slider
+          cubeFace.rotateX(-Math.PI / 2);
+          cubeFace.position.y = margin;
+          break;
+        case 3: // svg
+          cubeFace.position.z = margin;
+          break;
+        case 4: // chart
+          cubeFace.rotateX(-Math.PI);
+          cubeFace.position.z = -margin;
+          break;
+        case 5: // todo
+          cubeFace.rotateX(Math.PI / 2);
+          cubeFace.position.y = -margin;
+          break;
+        default:
+          break;
+      }
+
+      cube.add(cubeFace);
+    }
+
+    // Add cube to the entity
+    entity.addRenderable(cube);
+
+    // Cube rotation controls
+    document.querySelector('.control-btn.left').addEventListener('click', function() {
+      // rotate 90 degrees counter-clockwise around z axis
+      cube.rotateZ(Math.PI / 2);
+    });
+
+    document.querySelector('.control-btn.right').addEventListener('click', function() {
+      // rotate 90 degrees clockwise around z axis
+      cubbe.rotateZ(-Math.PI / 2);
+    });
+
+    document.querySelector('.control-btn.reset').addEventListener('click', function() {
+      // reset the cube's rotation
+      cube.quaternion = new Quaternion();
+    });
+  }).catch(function(error) {
+    // in case of loading error, do something
+    console.log(error);
+  });
+});
